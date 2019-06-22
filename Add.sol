bcfc77866b42bc3bdd638bf50bbfdf8453337691
GnanaLakshmi08 <48204576+GnanaLakshmi08@users.noreply.github.com>
2019-06-22 17:31:13 +0530

    Create Add.sol
    Day1: Write a Solidity Smart contract to add two numbers.
    https://youtu.be/Menh9yLIJn8

---
+++ Add.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract Add {
+    uint a;
+    uint b;
+    
+    constructor (uint _a, uint _b) public{
+        a = _a;
+        b = _b;
+    }
+    
+    function add() public view returns(uint){
+        uint num3 = a+b;
+        return num3;
+    }
+}
